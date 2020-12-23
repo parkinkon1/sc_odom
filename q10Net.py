7b3daf7bdcc440d803e37925824b21797332694a
q10park <parkinkon1@gmail.com>
2020-12-24 01:10:52 +0900

    fin

--- q10Net.py
+++ q10Net.py
@@ -3,10 +3,32 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from keras.applications import MobileNetV2, ResNet50
 import pickle
+import pandas as pd
+from keras.applications import MobileNetV2, ResNet50, InceptionV3
+
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import backend as K
+from keras.optimizers import SGD
+from keras.layers import Input
 
 np.random.seed(777)
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Restrict TensorFlow to only use the fourth GPU
+        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        print(e)  # Memory growth must be set before GPUs have been initialized
 
 
 class KittiLoader(tf.keras.utils.Sequence):
@@ -19,9 +41,9 @@ class KittiLoader(tf.keras.utils.Sequence):
         self.data_type = data_type
         self.index = np.arange(50, self.len) if not shuffle else np.random.permutation(np.arange(50, self.len))
         if data_type == 'train':
-            self.index = self.index[:int((self.len-50) * 0.7)]
+            self.index = self.index[:int((self.len - 50) * 0.7)]
         elif data_type == 'val':
-            self.index = self.index[int((self.len-50) * 0.7):]
+            self.index = self.index[int((self.len - 50) * 0.7):]
         elif data_type == 'test':
             pass
 
@@ -86,54 +108,167 @@ class KittiLoader(tf.keras.utils.Sequence):
         return X[np.newaxis, ...]
 
 
-def train():
-    img_shape = (720, 320, 2)
+class Q10Net:
+    def __init__(self, model_type='resnet50', input_shape=(320, 720, 2)):
+        # model
+        self.base_model = None
+        if model_type == 'inceptionv3':
+            self.base_model = InceptionV3(input_tensor=Input(shape=input_shape), weights=None, include_top=False)
+        elif model_type == 'resnet50':
+            self.base_model = ResNet50(input_tensor=Input(shape=input_shape), weights=None, include_top=False)
+        elif model_type == 'mobilenetv2':
+            self.base_model = MobileNetV2(input_tensor=Input(shape=input_shape), weights=None, include_top=False)
+        else:
+            print('no model found')
+            return
 
-    base_model = MobileNetV2(input_shape=img_shape, include_top=False, weights=None)
+        # self-supervised
+        x1 = self.base_model.output
+        x1 = GlobalAveragePooling2D()(x1)
+        x1 = Dense(1024, activation='relu')(x1)
+        self.self_predictions = Dense(6)(x1)
 
-    feature_batch = base_model(image_batch)
-    print(feature_batch.shape)
+        # real
+        x2 = self.base_model.output
+        x2 = GlobalAveragePooling2D()(x2)
+        x2 = Dense(1024, activation='relu')(x2)
+        self.predictions = Dense(2)(x2)
 
-    base_model.trainable = False
+        self.self_model = Model(inputs=self.base_model.input, outputs=self.self_predictions)
+        self.model = Model(inputs=self.base_model.input, outputs=self.predictions)
 
-    base_model.summary()
+        self.self_history = []
+        self.history = []
+        self.tune_history = []
+        self.tune2_history = []
 
-    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-    feature_batch_average = global_average_layer(feature_batch)
-    print(feature_batch_average.shape)
+    def train_self(self, train_loader, val_loader, epochs):
+        train_steps = train_loader.get_steps()
+        valid_steps = val_loader.get_steps()
+        for layer in self.base_model.layers:
+            layer.trainable = True
+        self.self_model.compile(optimizer='rmsprop', loss='mse', metrics=['mae', 'mse'])
+        self.self_history = self.self_model.fit_generator(train_loader, validation_data=val_loader,
+                                                     steps_per_epoch=train_steps, validation_steps=valid_steps,
+                                                     epochs=epochs)
+        plot_history(self.self_history)
 
-    prediction_layer = keras.layers.Dense(1)
-    prediction_batch = prediction_layer(feature_batch_average)
-    print(prediction_batch.shape)
+    def train_real(self, train_loader, val_loader, epochs):
+        train_steps = train_loader.get_steps()
+        valid_steps = val_loader.get_steps()
+        for layer in self.base_model.layers:
+            layer.trainable = True
+        self.model.compile(optimizer='rmsprop', loss='mse', metrics=['mae', 'mse'])
+        self.history = self.model.fit_generator(train_loader, validation_data=val_loader,
+                                                steps_per_epoch=train_steps, validation_steps=valid_steps,
+                                                epochs=epochs)
+        plot_history(self.history)
 
-    model = tf.keras.Sequential([
-        base_model,
-        global_average_layer,
-        prediction_layer
-    ])
+    def fine_tune(self, train_loader, val_loader, epochs):
+        train_steps = train_loader.get_steps()
+        valid_steps = val_loader.get_steps()
+        for layer in self.base_model.layers:
+            layer.trainable = False
+        self.model.compile(optimizer='rmsprop', loss='mse', metrics=['mae', 'mse'])
+        self.tune_history = self.model.fit_generator(train_loader, validation_data=val_loader,
+                                                     steps_per_epoch=train_steps, validation_steps=valid_steps,
+                                                     epochs=epochs)
+        plot_history(self.tune_history)
 
-    base_learning_rate = 0.0001
-    model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+    def fine_tune2(self, train_loader, val_loader, epochs):
+        train_steps = train_loader.get_steps()
+        valid_steps = val_loader.get_steps()
+        for layer in self.base_model.layers:
+            layer.trainable = True
+        self.model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='mse', metrics=['mae', 'mse'])
+        self.tune2_history = self.model.fit_generator(train_loader, validation_data=val_loader,
+                                                      steps_per_epoch=train_steps, validation_steps=valid_steps,
+                                                      epochs=epochs)
+        plot_history(self.tune2_history)
 
-    model.summary()
+    def eval(self, test_loader, save_dir=None):
+        loss, mae, mse = self.model.evaluate_generator(test_loader, verbose=2)
+        print("테스트 세트의 평균 절대 오차: {:5.2f}".format(mae))
+        print("테스트 세트의 mse: {:5.2f}".format(mse))
 
-    len(model.trainable_variables)
+        gt = []
+        for idx, batch in enumerate(test_loader):
+            gt.append(batch[1])
+        gt = np.reshape(np.array(gt), (-1, 2))
+        predicted = self.model.predict_generator(test_loader, steps=test_loader.get_steps())
+
+        gt_yaw = gt[:, 0]
+        gt_dis = gt[:, 1]
+        predicted_yaw = predicted[:, 0]
+        predicted_dis = predicted[:, 1]
+
+        plt.figure(figsize=(18, 4))
+
+        plt.subplot(1, 2, 1)
+        plt.title('Yaw Rates (rad/s)')
+        plt.xlabel('Sequence')
+        plt.ylabel('Yaw rate (rad/s)')
+        plt.plot(range(len(gt_yaw)), gt_yaw, label='gt_yaw')
+        plt.plot(range(len(predicted_yaw)), predicted_yaw, label='predicted_yaw')
+        plt.legend(loc='upper right')
+
+        plt.subplot(1, 2, 2)
+        plt.title('Forward Velocities (m/s)')
+        plt.xlabel('Sequence')
+        plt.ylabel('Forward velocity (m/s)')
+        plt.plot(range(len(gt_dis)), gt_dis, label='gt_dis')
+        plt.plot(range(len(predicted_dis)), predicted_dis, label='predicted_dis')
+        plt.legend(loc='upper right')
+        plt.show()
+
+        plt.figure(figsize=(18, 4))
+
+        plt.subplot(1, 2, 1)
+        plt.title('Error Distribution: Yaw Rates (rad/s)')
+        error = predicted_yaw - gt_yaw
+        plt.hist(error, bins=25)
+        plt.xlabel("Prediction Error [MPG]")
+        plt.ylabel("Count")
+
+        plt.subplot(1, 2, 2)
+        plt.title('Error Distribution: Forward Velocities (m/s)')
+        error = predicted_dis - gt_dis
+        plt.hist(error, bins=25)
+        plt.xlabel("Prediction Error [MPG]")
+        plt.ylabel("Count")
+        plt.show()
+
+        if save_dir is not None:
+            data = [gt, predicted]
+            with open(save_dir, 'wb') as file:
+                pickle.dump(data, file)
 
 
+def plot_history(history):
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
 
+    plt.figure(figsize=(18, 4))
 
+    plt.subplot(1, 2, 1)
+    plt.title('Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.plot(hist['epoch'], hist['loss'],
+             label='Train loss')
+    plt.plot(hist['epoch'], hist['val_loss'],
+             label='Val loss')
+    # plt.ylim([0, 20])
+    plt.legend(loc='upper right')
 
-
-
-
-
-
-
-
-
-
-
-
-
+    plt.subplot(1, 2, 2)
+    plt.title('MSE')
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Square Error [$MPG^2$]')
+    plt.plot(hist['epoch'], hist['mse'],
+             label='Train Error')
+    plt.plot(hist['epoch'], hist['val_mse'],
+             label='Val Error')
+    # plt.ylim([0, 20])
+    plt.legend(loc='upper right')
+    plt.show()
--- sc_generation.py
+++ sc_generation.py
@@ -8,6 +8,11 @@ from utils import lla_to_enu
 import pykitti
 import pickle
 from tqdm import tqdm
+import math
+from tensorflow.keras.utils import Sequence
+import keras
+import cv2
+from scipy.spatial.transform import Rotation as R
 
 np.random.seed(777)
 
@@ -139,6 +144,262 @@ def kitti2sc(kitti_root_dir='/datasets/kitti/raw', kitti_date='2011_09_30', kitt
         out_name = '{}/{}.bin'.format(out_dir, idx)
         with open(out_name, 'wb') as file:
             pickle.dump(data, file)
+
+
+class KittiParser:
+    def __init__(self, kitti_root_dir='/datasets/kitti/raw', kitti_date='2011_09_30', kitti_drive='0033'):
+        self.gt_trajectory_lla = []
+        self.gt_yaws = []
+        self.gt_yaw_rates = []
+        self.gt_forward_velocities = []
+        self.ts = []
+        self.gt_trajectory_xyz = []
+        self.lidar_points = []
+
+        self.kitti_date = kitti_date
+        self.kitti_drive = kitti_drive
+
+        self.load_kitti(kitti_root_dir=kitti_root_dir, kitti_date=kitti_date, kitti_drive=kitti_drive)
+
+        pass
+
+    def load_kitti(self, kitti_root_dir='/datasets/kitti/raw', kitti_date='2011_09_30', kitti_drive='0033'):
+        # kitti dataset
+        dataset = pykitti.raw(kitti_root_dir, kitti_date, kitti_drive)
+
+        gt_trajectory_lla = []  # [longitude(deg), latitude(deg), altitude(meter)] x N
+        gt_yaws = []  # [yaw_angle(rad),] x N
+        gt_yaw_rates = []  # [vehicle_yaw_rate(rad/s),] x N
+        gt_forward_velocities = []  # [vehicle_forward_velocity(m/s),] x N
+
+        for oxts_data in dataset.oxts:
+            packet = oxts_data.packet
+            gt_trajectory_lla.append([
+                packet.lon,
+                packet.lat,
+                packet.alt
+            ])
+            gt_yaws.append(packet.yaw)
+            gt_yaw_rates.append(packet.wz)
+            gt_forward_velocities.append(packet.vf)
+
+        self.gt_trajectory_lla = np.array(gt_trajectory_lla).T
+        self.gt_yaws = np.array(gt_yaws)
+        self.gt_yaw_rates = np.array(gt_yaw_rates)
+        self.gt_forward_velocities = np.array(gt_forward_velocities)
+
+        timestamps = np.array(dataset.timestamps)
+        elapsed = np.array(timestamps) - timestamps[0]
+        self.ts = np.array([t.total_seconds() for t in elapsed])
+
+        # pre-processing: gps pose
+        origin = self.gt_trajectory_lla[:, 0]  # set the initial position to the origin
+        self.gt_trajectory_xyz = lla_to_enu(self.gt_trajectory_lla, origin)
+
+        self.lidar_points = dataset.velo
+
+        print('kitti dataset loaded: {}_{}_{}'.format(kitti_root_dir, kitti_date, kitti_drive))
+
+    def saveSC(self, sector_res=720, ring_res=320):
+        down_cell_size = 0.25
+        kitti_lidar_height = 2.0
+        sector_res = sector_res  # rotation
+        ring_res = ring_res  # velocity
+        max_length = 80
+
+        out_dir = './data/scs/{}_{}'.format(self.kitti_date, self.kitti_drive)
+        if os.path.isdir(out_dir):
+            print('SC data already exists')
+            return False
+        else:
+            os.mkdir(out_dir)
+            for idx, cloud in tqdm(enumerate(self.lidar_points), total=len(self.ts), desc='generating scs'):
+                cloud_xyz = cloud[:, :3]
+                # down-sampling
+                pcd = PointCloud()
+                pcd.points = Vector3dVector(cloud_xyz)
+                down_pcd = voxel_down_sample(pcd, voxel_size=down_cell_size)
+                cloud_xyz_downed = np.asarray(down_pcd.points)
+                # make SC
+                sc = cloud2sc(cloud_xyz_downed, sector_res, ring_res, max_length, kitti_lidar_height)
+                # save
+                out_name = '{}/{}.bin'.format(out_dir, idx)
+                with open(out_name, 'wb') as file:
+                    pickle.dump(sc, file)
+            return True
+
+    def saveSC_aug_with_label(self, sector_res=720, ring_res=320):
+        down_cell_size = 0.25
+        kitti_lidar_height = 2.0
+        sector_res = sector_res  # rotation
+        ring_res = ring_res  # velocity
+        max_length = 80
+
+        out_dir = './data/sc_aug/{}_{}'.format(self.kitti_date, self.kitti_drive)
+        if os.path.isdir(out_dir):
+            print('SC(augmented) data already exists')
+            return False
+        else:
+            os.mkdir(out_dir)
+            for idx, cloud in tqdm(enumerate(self.lidar_points), total=len(self.ts), desc='generating scs_aug'):
+                cloud_xyz = cloud[:, :3]
+                # down-sampling
+                pcd = PointCloud()
+                pcd.points = Vector3dVector(cloud_xyz)
+                down_pcd = voxel_down_sample(pcd, voxel_size=down_cell_size)
+                cloud_xyz_downed = np.asarray(down_pcd.points)
+                # augmentation
+                rot_x = np.pi/2 * (np.random.rand() - 0.5) * 0.1
+                rot_y = np.pi/2 * (np.random.rand() - 0.5) * 0.1
+                rot_z = np.pi/2 * (np.random.rand() - 0.5) * 1.0
+                r = R.from_rotvec(np.array([rot_x, rot_y, rot_z]))
+                t = (np.random.rand(3) * 6) - 3.0
+                cloud_rot = r.apply(cloud_xyz_downed)
+                cloud_trans = cloud_xyz_downed + t
+                # make SC
+                sc = cloud2sc(cloud_xyz_downed, sector_res, ring_res, max_length, kitti_lidar_height)
+                sc_rot = cloud2sc(cloud_rot, sector_res, ring_res, max_length, kitti_lidar_height)
+                sc_trans = cloud2sc(cloud_trans, sector_res, ring_res, max_length, kitti_lidar_height)
+                label_rot = np.append(r.as_rotvec(), np.array([0., 0., 0.]), axis=0)
+                label_trans = np.append(np.array([0., 0., 0.]), t, axis=0)
+                data = [sc, sc_rot, sc_trans, label_rot, label_trans]
+                # save
+                out_name = '{}/{}.bin'.format(out_dir, idx)
+                with open(out_name, 'wb') as file:
+                    pickle.dump(data, file)
+            return True
+
+    def saveSC_with_label(self):
+        out_dir = './data/sc_with_label/{}_{}'.format(self.kitti_date, self.kitti_drive)
+        sc_dir = self.get_sc_dir()
+        if os.path.isdir(out_dir):
+            print('SC with label dataset already exists')
+            return False
+        else:
+            os.mkdir(out_dir)
+            for idx in tqdm(range(len(self.ts)), total=len(self.ts), desc='generating scs_labels'):
+                idx_10 = idx - 10
+                if idx_10 < 0:
+                    idx_10 = 0
+                sc0_path = '{}/{}.bin'.format(sc_dir, idx)
+                sc10_path = '{}/{}.bin'.format(sc_dir, idx_10)
+                with open(sc0_path, 'rb') as f:
+                    sc0 = pickle.load(f)
+                with open(sc10_path, 'rb') as f:
+                    sc10 = pickle.load(f)
+
+                dt = self.ts[idx] - self.ts[idx_10]
+                yaw_diff = self.gt_yaw_rates[idx_10] * dt
+                dis_diff = self.gt_forward_velocities[idx_10] * dt
+
+                data = [sc0, sc10, yaw_diff, dis_diff]
+                # save
+                out_name = '{}/{}.bin'.format(out_dir, idx)
+                with open(out_name, 'wb') as file:
+                    pickle.dump(data, file)
+            return True
+
+    def get_num_data(self):
+        return len(self.ts)
+
+    def get_sc_dir(self):
+        return './data/scs/{}_{}'.format(self.kitti_date, self.kitti_drive)
+
+    def get_sc_with_label_dir(self):
+        return './data/sc_with_label/{}_{}'.format(self.kitti_date, self.kitti_drive)
+
+    def get_aug_with_label_dir(self):
+        return './data/sc_aug/{}_{}'.format(self.kitti_date, self.kitti_drive)
+
+
+class Loader(keras.utils.Sequence):
+    def __init__(self, kitti_root_dir, kitti_date, kitti_drive, data_type='raw', batch_size=10, shuffle=True, load_type='train', val_ratio=0.0):
+        np.random.seed(777)
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+        self.kitti_dataset = KittiParser(kitti_root_dir=kitti_root_dir, kitti_date=kitti_date, kitti_drive=kitti_drive)
+        self.data_type = data_type
+
+        self.ids = np.arange(self.kitti_dataset.get_num_data())
+        self.data_len = len(self.ids)
+
+        # split train val
+        train_size = int(self.data_len * (1 - val_ratio))
+        if self.shuffle:
+            train_ids = np.random.choice(self.ids, train_size, replace=False)
+            val_ids = np.setdiff1d(self.ids, train_ids)
+        else:
+            train_ids = self.ids[:train_size]
+            val_ids = self.ids[train_size:]
+
+        if load_type == 'train':
+            self.ids = train_ids
+        elif load_type == 'val':
+            self.ids = val_ids
+        else:
+            pass
+
+        self.data_len = len(self.ids)
+        self.indexes = np.arange(len(self.ids))
+
+        self.sc_dir = self.kitti_dataset.get_sc_dir()
+        self.sc_with_label_dir = self.kitti_dataset.get_sc_with_label_dir()
+
+        self.data_dir = None
+        if self.data_type == 'raw':
+            self.data_dir = self.kitti_dataset.get_sc_with_label_dir()
+        elif self.data_type == 'aug':
+            self.data_dir = self.kitti_dataset.get_aug_with_label_dir()
+
+        self.on_epoch_end()
+
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.ids))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation__(self, id_name):
+        data_path = '{}/{}.bin'.format(self.data_dir, id_name)
+        with open(data_path, 'rb') as f:
+            data = pickle.load(f)
+
+        if self.data_type == 'raw':
+            sc0, sc10, yaw_diff, dis_diff = data
+            x = np.concatenate((sc0[:, :, np.newaxis], sc10[:, :, np.newaxis]), axis=2)
+            y = np.array([yaw_diff, dis_diff])
+            return x, y
+        elif self.data_type == 'aug':
+            sc, sc_rot, sc_trans, label_rot, label_trans = data
+            if id_name % 2 == 0:
+                x = np.concatenate((sc[:, :, np.newaxis], sc_rot[:, :, np.newaxis]), axis=2)
+                y = label_rot
+                return x, y
+            else:
+                x = np.concatenate((sc[:, :, np.newaxis], sc_trans[:, :, np.newaxis]), axis=2)
+                y = label_trans
+                return x, y
+
+    def __len__(self):
+        return int(np.floor(self.data_len / self.batch_size))
+
+    def __getitem__(self, index):  # index : batch no.
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+        batch_ids = [self.ids[k] for k in indexes]
+
+        imgs = list()
+        labels = list()
+        for id_name in batch_ids:
+            x, y = self.__data_generation__(id_name)
+            imgs.append(x)
+            labels.append(y)
+        imgs = np.array(imgs)
+        labels = np.array(labels)
+
+        return imgs, labels  # return batch
+
+    def get_steps(self):
+        return self.data_len // self.batch_size
 
 
 if __name__ == "__main__":
